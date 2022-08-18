0056dbab94c9062f93feee51ba24254e4e84e75c
ecyht2 <ecyht2@nottingham.edu.my>
2022-08-18 15:30:53 +0800

    Rewrote UIMainWindow initialization

--- scripts/update.py
+++ scripts/update.py
@@ -20,6 +20,7 @@ from iot.msg import DrawerContent
 
 from hmi_modules.ctrl import Ui_ControlWindow
 from hmi_modules.analoggaugewidget import QRoundProgressBar
+from hmi_modules.QRDetector import QRDetector
 
 
 class UIMainWindow(QtWidgets.QMainWindow):  # object
@@ -31,14 +32,14 @@ class UIMainWindow(QtWidgets.QMainWindow):  # object
         self.window.show()
         self.close()
 
-    def setupUi(self, MainWindow):
-        super().__init__()
+    def __init__(self, parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
 
         # rospy.init_node('send_client_goal')
         self.client = actionlib.SimpleActionClient('/move_base',
                                                    MoveBaseAction)
         # self.client.wait_for_server()
-        # MainWindow.showFullScreen()
+        # self.showFullScreen()
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.pub_nav = rospy.Publisher('/move_base/cancel', GoalID,
                                        queue_size=10)
@@ -54,190 +55,28 @@ class UIMainWindow(QtWidgets.QMainWindow):  # object
         self.pub_drawer = rospy.Publisher("iot/drawer", DrawerContent,
                                           queue_size=10)
 
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1280, 720)
-        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonTextOnly)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.setObjectName("MainWindow")
+        self.setToolButtonStyle(QtCore.Qt.ToolButtonTextOnly)
+        self.central_widget = QtWidgets.QStackedWidget(self)
 
-        # Nottingham Logo
-        self.myLogo = QtWidgets.QLabel(self.centralwidget)
-        self.myLogo.setGeometry(QtCore.QRect(50, 10, 281, 121))
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        self.myLogo.setFont(font)
-        self.myLogo.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.myLogo.setLineWidth(0)
-        self.myLogo.setText("")
-        self.myLogo.setPixmap(QtGui.QPixmap(":/newicon/icons/nott.png"))
-        self.myLogo.setObjectName("myLogo")
+        self.__make_main_widget()
 
-        # Button to go to trent
-        self.myCall = QtWidgets.QPushButton(self.centralwidget)
-        self.myCall.setGeometry(QtCore.QRect(50, 130, 281, 141))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.myCall.setFont(font)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/newicon/icons/trent.png"),
-                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.myCall.setIcon(icon)
-        self.myCall.setIconSize(QtCore.QSize(40, 40))
-        self.myCall.setObjectName("Send")
-        self.myCall.clicked.connect(self.send)
-
-        # Button to go to D block
-        self.myHome = QtWidgets.QPushButton(self.centralwidget)
-        self.myHome.setGeometry(QtCore.QRect(50, 300, 281, 141))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.myHome.setFont(font)
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(":/newicon/icons/home.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.myHome.setIcon(icon1)
-        self.myHome.setIconSize(QtCore.QSize(40, 40))
-        self.myHome.setObjectName("myHome")
-        self.myHome.show()
-        self.myHome.clicked.connect(self.recive)
-
-        # Manual Controller for the AMD-R
-        self.myManual = QtWidgets.QPushButton(self.centralwidget)
-        self.myManual.setGeometry(QtCore.QRect(950, 315, 231, 171))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.myManual.setFont(font)
-        self.myManual.setText("")
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(":/newicon/icons/control.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.myManual.setIcon(icon2)
-        self.myManual.setIconSize(QtCore.QSize(150, 150))
-        self.myManual.setObjectName("myManual")
-        self.myManual.clicked.connect(self.openWindow)
-
-        # Progress bar for battery percentage
-        self.widget = QRoundProgressBar(self.centralwidget)
-        self.widget.setGeometry(QtCore.QRect(535, 140, 261, 221))  # 321, 261))
-        # self.widget.setAlignment(Qt.AlignCenter)
-        self.widget.setObjectName("widget")
-
-        # Autoscrolling Logger
-        self.widget_2 = QtWidgets.QWidget(self.centralwidget)
-        self.widget_2.setGeometry(QtCore.QRect(360, 370, 561, 241))
-        self.widget_2.setObjectName("widget_2")
-        self.logText = QtWidgets.QLabel(self.widget_2)
-        self.ScrollLabel = QtWidgets.QScrollArea(self.widget_2)
-        # Making Widget the desired size
-        self.ScrollLabel.setGeometry(QtCore.QRect(0, 0, 561, 241))
-        self.label_scroll = QtWidgets.QLabel(self.ScrollLabel)  # ??? ######
-        self.testtext = "Logging..."
-        self.label_scroll.setText(self.testtext)
-        self.label_scroll.setGeometry(QtCore.QRect(0, 0, 561, 241))
-        self.label_scroll_counter = QtWidgets.QLabel(self.ScrollLabel)
-        self.ScrollLabel.setWidgetResizable(True)
-        self.lay = QtWidgets.QVBoxLayout(self.widget_2)
-        self.label_scroll.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        self.label_scroll.setWordWrap(True)
-        self.lay.addWidget(self.label_scroll)
-        self.ScrollLabel.setWidget(self.label_scroll)
-        self.mybar = self.ScrollLabel.verticalScrollBar()
-        self.mybar.setValue(self.mybar.maximum())
-        self.widget_2.show()
-
-        # Emergency Stop button
-        self.myStop = QtWidgets.QPushButton(self.centralwidget)
-        self.myStop.setGeometry(QtCore.QRect(950, 130, 231, 171))
-        self.myStop.setStyleSheet('QPushButton'
-                                  '{background-color: #A3C1DA; color: red;}')
-        # self.myStop.setStyleSheet("background-color: rgb(255, 0, 0);")
-        font = QtGui.QFont()
-        font.setFamily("MS Shell Dlg 2")
-        font.setPointSize(20)
-        font.setBold(False)
-        font.setItalic(False)
-        font.setWeight(50)
-        self.myStop.setFont(font)
-        self.myStop.setStyleSheet("")
-        self.myStop.setText("")
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(":/newicon/icons/stop.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.On)
-        self.myStop.setIcon(icon2)
-        self.myStop.setIconSize(QtCore.QSize(500, 500))
-        # self.myStop.setStyleSheet("background-color: rgb(255, 0, 0);")
-        self.myStop.setObjectName("myStop")
-        self.myStop.clicked.connect(self.stop)
-
-        # Current Time Label
-        self.myTime = QtWidgets.QLabel(self.centralwidget)
-        self.myTime.setGeometry(QtCore.QRect(950, 50, 231, 61))
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        self.myTime.setFont(font)
-        self.myTime.setAlignment(Qt.AlignCenter)
-        self.myTime.setObjectName("myTime")
-
-        # Current Mission Label
-        self.myMission = QtWidgets.QLabel(self.centralwidget)
-        self.myMission.setGeometry(QtCore.QRect(460, 20, 400, 91))
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
-                                           QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.myMission.sizePolicy()
-                                     .hasHeightForWidth())
-        self.myMission.setSizePolicy(sizePolicy)
-        font = QtGui.QFont()
-        font.setPointSize(20)
-        self.myMission.setFont(font)
-        self.myMission.setObjectName("myMission")
-        # self.myMission.setText(self.label_scroll.text)
-
-        # Battery Level Label
-        self.myBatteryLevel = QtWidgets.QLabel(self.centralwidget)
-        self.myBatteryLevel.setGeometry(QtCore.QRect(545, 110, 181, 16))
-        self.myBatteryLevel.setAlignment(Qt.AlignCenter)
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.myBatteryLevel.setFont(font)
-        self.myBatteryLevel.setObjectName("myBatteryLevel")
-        self.myReset = QtWidgets.QPushButton(self.centralwidget)
-        self.myReset.setGeometry(QtCore.QRect(50, 470, 281, 141))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        self.myReset.setFont(font)
-        self.myReset.setObjectName("myReset")
-        self.myReset.clicked.connect(self.demo_arm)
-
-        # Quit Button
-        self.myExit = QtWidgets.QPushButton(self.centralwidget)
-        self.myExit.setGeometry(QtCore.QRect(1170, 0, 91, 61))
-        icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap(":/newicon/icons/exit.png"),
-                        QtGui.QIcon.Normal, QtGui.QIcon.On)
-        self.myExit.setIcon(icon3)
-        self.myExit.setIconSize(QtCore.QSize(60, 60))
-        self.myExit.clicked.connect(self.exitapp)
-        self.myExit.setObjectName("myExit")
-
-        self.myDesc = QtWidgets.QLabel(self.centralwidget)
-        self.myDesc.setGeometry(QtCore.QRect(950, 500, 231, 111))
-        self.myDesc.setFrameShape(QtWidgets.QFrame.Box)
-        self.myDesc.setObjectName("myDesc")
+        # Adding widgets to stacked widget
+        self.central_widget.addWidget(self.main_screen)
+        self.central_widget.addWidget(self.qr_reader)
 
         # Status Bar
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.setCentralWidget(self.central_widget)
+        self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1269, 26))
         self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
         self.logged_count = 0
         # myList[1] = "Logging Window"
 
@@ -480,7 +319,7 @@ class UIMainWindow(QtWidgets.QMainWindow):  # object
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
         # self.label.setText(_translate("MainWindow", "ANDY's MAIL ROBOT"))
         # self.myStop.setText(_translate("MainWindow", "STOP"))
         self.myCall.setText(_translate("MainWindow", " GOAL"))
@@ -498,6 +337,177 @@ class UIMainWindow(QtWidgets.QMainWindow):  # object
                                        "Kenji Eu\n"
                                        "Chia Yu Hang\n"
                                        "Neo Jie En"))
+
+    def __make_main_widget(self) -> None:
+        """Function to make main widget."""
+        # Creating main_screen widget
+        self.main_screen = QtWidgets.QWidget(self)
+        self.main_screen.setObjectName("main_screen")
+
+        # Nottingham Logo
+        self.myLogo = QtWidgets.QLabel(self.main_screen)
+        self.myLogo.setGeometry(QtCore.QRect(50, 10, 281, 121))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.myLogo.setFont(font)
+        self.myLogo.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.myLogo.setLineWidth(0)
+        self.myLogo.setPixmap(QtGui.QPixmap(":/newicon/icons/nott.png"))
+        self.myLogo.setObjectName("myLogo")
+
+        # Button to go to trent
+        self.myCall = QtWidgets.QPushButton(self.main_screen)
+        self.myCall.setGeometry(QtCore.QRect(50, 130, 281, 141))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.myCall.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/newicon/icons/trent.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.myCall.setIcon(icon)
+        self.myCall.setIconSize(QtCore.QSize(40, 40))
+        self.myCall.setObjectName("Send")
+        self.myCall.clicked.connect(self.send)
+
+        # Button to go to D block
+        self.myHome = QtWidgets.QPushButton(self.main_screen)
+        self.myHome.setGeometry(QtCore.QRect(50, 300, 281, 141))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.myHome.setFont(font)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/newicon/icons/home.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.myHome.setIcon(icon1)
+        self.myHome.setIconSize(QtCore.QSize(40, 40))
+        self.myHome.setObjectName("myHome")
+        self.myHome.show()
+        self.myHome.clicked.connect(self.recive)
+
+        # Manual Controller for the AMD-R
+        self.myManual = QtWidgets.QPushButton(self.main_screen)
+        self.myManual.setGeometry(QtCore.QRect(950, 315, 231, 171))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.myManual.setFont(font)
+        self.myManual.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/newicon/icons/control.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.myManual.setIcon(icon2)
+        self.myManual.setIconSize(QtCore.QSize(150, 150))
+        self.myManual.setObjectName("myManual")
+        self.myManual.clicked.connect(self.openWindow)
+
+        # Progress bar for battery percentage
+        self.widget = QRoundProgressBar(self.main_screen)
+        self.widget.setGeometry(QtCore.QRect(535, 140, 261, 221))  # 321, 261))
+        # self.widget.setAlignment(Qt.AlignCenter)
+        self.widget.setObjectName("widget")
+
+        # Autoscrolling Logger
+        self.widget_2 = QtWidgets.QWidget(self.main_screen)
+        self.widget_2.setGeometry(QtCore.QRect(360, 370, 561, 241))
+        self.widget_2.setObjectName("widget_2")
+        self.logText = QtWidgets.QLabel(self.widget_2)
+        self.ScrollLabel = QtWidgets.QScrollArea(self.widget_2)
+        # Making Widget the desired size
+        self.ScrollLabel.setGeometry(QtCore.QRect(0, 0, 561, 241))
+        self.label_scroll = QtWidgets.QLabel(self.ScrollLabel)  # ??? ######
+        self.testtext = "Logging..."
+        self.label_scroll.setText(self.testtext)
+        self.label_scroll.setGeometry(QtCore.QRect(0, 0, 561, 241))
+        self.label_scroll_counter = QtWidgets.QLabel(self.ScrollLabel)
+        self.ScrollLabel.setWidgetResizable(True)
+        self.lay = QtWidgets.QVBoxLayout(self.widget_2)
+        self.label_scroll.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.label_scroll.setWordWrap(True)
+        self.lay.addWidget(self.label_scroll)
+        self.ScrollLabel.setWidget(self.label_scroll)
+        self.mybar = self.ScrollLabel.verticalScrollBar()
+        self.mybar.setValue(self.mybar.maximum())
+        self.widget_2.show()
+
+        # Emergency Stop button
+        self.myStop = QtWidgets.QPushButton(self.main_screen)
+        self.myStop.setGeometry(QtCore.QRect(950, 130, 231, 171))
+        self.myStop.setStyleSheet('QPushButton'
+                                  '{background-color: #A3C1DA; color: red;}')
+        # self.myStop.setStyleSheet("background-color: rgb(255, 0, 0);")
+        font = QtGui.QFont()
+        font.setFamily("MS Shell Dlg 2")
+        font.setPointSize(20)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        self.myStop.setFont(font)
+        self.myStop.setStyleSheet("")
+        self.myStop.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/newicon/icons/stop.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.myStop.setIcon(icon2)
+        self.myStop.setIconSize(QtCore.QSize(500, 500))
+        # self.myStop.setStyleSheet("background-color: rgb(255, 0, 0);")
+        self.myStop.setObjectName("myStop")
+        self.myStop.clicked.connect(self.stop)
+
+        # Current Time Label
+        self.myTime = QtWidgets.QLabel(self.main_screen)
+        self.myTime.setGeometry(QtCore.QRect(950, 50, 231, 61))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.myTime.setFont(font)
+        self.myTime.setAlignment(Qt.AlignCenter)
+        self.myTime.setObjectName("myTime")
+
+        # Current Mission Label
+        self.myMission = QtWidgets.QLabel(self.main_screen)
+        self.myMission.setGeometry(QtCore.QRect(460, 20, 400, 91))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred,
+                                           QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.myMission.sizePolicy()
+                                     .hasHeightForWidth())
+        self.myMission.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.myMission.setFont(font)
+        self.myMission.setObjectName("myMission")
+        # self.myMission.setText(self.label_scroll.text)
+
+        # Battery Level Label
+        self.myBatteryLevel = QtWidgets.QLabel(self.main_screen)
+        self.myBatteryLevel.setGeometry(QtCore.QRect(545, 110, 181, 16))
+        self.myBatteryLevel.setAlignment(Qt.AlignCenter)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.myBatteryLevel.setFont(font)
+        self.myBatteryLevel.setObjectName("myBatteryLevel")
+        self.myReset = QtWidgets.QPushButton(self.main_screen)
+        self.myReset.setGeometry(QtCore.QRect(50, 470, 281, 141))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.myReset.setFont(font)
+        self.myReset.setObjectName("myReset")
+        self.myReset.clicked.connect(self.demo_arm)
+
+        # Quit Button
+        self.myExit = QtWidgets.QPushButton(self.main_screen)
+        self.myExit.setGeometry(QtCore.QRect(1170, 0, 91, 61))
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(":/newicon/icons/exit.png"),
+                        QtGui.QIcon.Normal, QtGui.QIcon.On)
+        self.myExit.setIcon(icon3)
+        self.myExit.setIconSize(QtCore.QSize(60, 60))
+        self.myExit.clicked.connect(self.exitapp)
+        self.myExit.setObjectName("myExit")
+
+        self.myDesc = QtWidgets.QLabel(self.main_screen)
+        self.myDesc.setGeometry(QtCore.QRect(950, 500, 231, 111))
+        self.myDesc.setFrameShape(QtWidgets.QFrame.Box)
+        self.myDesc.setObjectName("myDesc")
 
 
 def batteryTemp(data: BatteryState, ui: UIMainWindow) -> None:
@@ -540,10 +550,8 @@ if __name__ == "__main__":
 
         # Creating UI object
         app = QtWidgets.QApplication(sys.argv)
-        MainWindow = QtWidgets.QMainWindow()
         ui = UIMainWindow()
-        ui.setupUi(MainWindow)
-        MainWindow.showFullScreen()
+        ui.showFullScreen()
         ui.updateMission("Idle")
 
         # Subscribing to topics
